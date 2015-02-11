5a9bfbe2c26a07dca9e595707d0fe463ac99a524
Bhagya Nirmaan Silva <bhagyasilva124@gmail.com>
2015-02-11 13:41:41 +0100

    Update install-qcad-ubuntu.sh

--- dwg-transform-support/install-qcad-ubuntu.sh
+++ dwg-transform-support/install-qcad-ubuntu.sh
@@ -1,3 +1,15 @@
+if [ "$(whoami)" != "root" ]; then
+        echo "*************************************"
+        echo "You need to run this script with sudo"
+        echo "*************************************"
+        echo "Sorry, you are not root."
+        exit 1
+fi
+
 wget https://www.ribbonsoft.com/archives/qcad/qcad-3.8.1-linux-x86_64.run
 chmod +x qcad-3.8.1-linux-x86_64.run
 ./qcad-3.8.1-linux-x86_64.run
+echo "Downloading Vector Transformations module wrappers..."
+wget https://raw.githubusercontent.com/loftuxab/alfresco-vector-transformations-module/master/dwg-transform-support/dwg2bmp_alfresco_fallback -P ~/opt/qcad-3.8.1-linux-x86_64/
+wget https://raw.githubusercontent.com/loftuxab/alfresco-vector-transformations-module/master/dwg-transform-support/dwg2svg_alfresco_fallback -P ~/opt/qcad-3.8.1-linux-x86_64/
+echo "QCad installation completed."
